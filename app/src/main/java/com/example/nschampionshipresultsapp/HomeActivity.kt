2e4ceb30ec1d9689cbb5a6080b8d998d6b5614da
Jonathan Santos | Stone <jonathanlbs@gmail.com>
2020-01-28 17:15:00 -0300

    Added menu

--- app/src/main/java/com/example/nschampionshipresultsapp/HomeActivity.kt
+++ app/src/main/java/com/example/nschampionshipresultsapp/HomeActivity.kt
@@ -3,19 +3,47 @@ package com.example.nschampionshipresultsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.toolbar.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        buttonLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(this, LoginActivity::class.java))
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Resultados NS"
+
+        var toggle = ActionBarDrawerToggle(this,drawer_layout,toolbar,R.string.open_drawer, R.string.close_drawer)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this,item.itemId, Toast.LENGTH_SHORT).show()
+        when (item.itemId){
+            R.id.nav_game -> {
+                startActivity(Intent(this, SignUpActivity::class.java))
+                //Toast.makeText(this,"Clicou no primeiro item", Toast.LENGTH_SHORT).show()
+            }
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
         }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
